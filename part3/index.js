887fb88927da09714a70e2a99f86a91e221c7fb0
W-oland <tatu.jarvenranta@gmail.com>
2021-08-11 09:07:05 +0300

    tehtiin tehtava 12

--- part3/index.js
+++ part3/index.js
@@ -1,12 +1,35 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+// const mongoose = require('mongoose')
+const Yhteystieto = require('./models/yhteystieto')
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
-let notes = [
+/* const url =
+  `mongodb+srv://fullstack:salasana@cluster0.kakmq.mongodb.net/uusiSovellus?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const yhteystietoSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+yhteystietoSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Yhteystieto = mongoose.model('Yhteystieto', yhteystietoSchema) */
+
+/* let notes = [
   {
     "name": "Tatu T",
     "number": "123",
@@ -32,14 +55,16 @@ let notes = [
     "number": "123123123",
     "id": 11
   }
-]
+] */
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  Yhteystieto.find({}).then(yhteystieto => {
+    res.json(yhteystieto)
+  })
 })
 
 const generateId = () => {
@@ -88,7 +113,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
