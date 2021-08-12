39fc8e53a4610d48170c1057a8f08c78ddc59500
W-oland <tatu.jarvenranta@gmail.com>
2021-08-12 07:44:16 +0300

    eslint tehtävät

--- part3/index.js
+++ part3/index.js
@@ -3,7 +3,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const Yhteystieto = require('./models/yhteystieto')
-const { response } = require('express')
 
 app.use(express.json())
 app.use(cors())
@@ -37,8 +36,22 @@ app.get('/api/info', (request, response) => {
   })
 })
 
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
 
-app.post('/api/notes', (request, response) => {
+  const yhteystieto = new Yhteystieto ({
+    name: body.name,
+    number: body.number
+  })
+
+  yhteystieto.save()
+    .then(saved => {
+      response.json(saved.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+/*app.post('/api/notes', (request, response) => {
   const body = request.body
   console.log(body)
 
@@ -56,14 +69,14 @@ app.post('/api/notes', (request, response) => {
   yhteystieto.save().then(savedNote => {
     response.json(savedNote)
   })
-})
+})*/
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Yhteystieto.findByIdAndRemove(request.params.id)
-  .then(result => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -89,7 +102,9 @@ app.listen(PORT, () => {
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if (error.name === 'CastError') {
-    return response.status(400).send({error: 'malformatted id'})
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
