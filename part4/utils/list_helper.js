de6fec75d52103817d7ea772c10c2742ff6a0ee8
W-oland <tatu.jarvenranta@gmail.com>
2021-08-17 12:26:53 +0300

    tehtava 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likes = blogs.map(blog => blog.likes)
+
+    const reducer = (accumulator, currentValue) => {
+        return accumulator + currentValue
+    }
+    return likes.reduce(reducer)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
