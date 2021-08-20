e951b45070c9e14021854747b836214b3dbf11b3
W-oland <tatu.jarvenranta@gmail.com>
2021-08-20 08:45:49 +0300

    tehtava 4.20

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -8,20 +8,20 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
-const getTokenFrom = request => {
+/*const getTokenFrom = request => {
     const authorization = request.get('authorization')
     if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
         return authorization.substring(7)
     }
     return null
-}
+}*/
 
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
+    //const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-    if (!token || !decodedToken.id) {
+    if (!request.token || !decodedToken.id) {
         return response.status(401).json({error: 'token missing or invalid'})
     }
     
