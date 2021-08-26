0548267de8b4bb8d8e794111f5e60ccee0828c0c
W-oland <tatu.jarvenranta@gmail.com>
2021-08-26 10:07:20 +0300

    tehtava 5.7

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -101,7 +101,7 @@ const App = () => {
     </Togglable>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
-      )}
+     )}
     </div>
   )
 }
--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Blog = ({blog}) => (
-  <div>
+const Blog = ({blog}) => {
+  const [showDetails, SETshowDetails] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const handleToggle = () => {
+    SETshowDetails(!showDetails)
+  }
+
+  return (
+  <div style={blogStyle}>
     {blog.title} {blog.author}
+    <button onClick={handleToggle}>{showDetails ? 'hide' : 'view'}</button>
+    {showDetails &&  (
+      <ul>
+        <li>{blog.url}</li>
+        <li>{blog.likes}
+          <button>like</button>
+        </li>
+        <li>{blog.user.name}</li>
+      </ul>
+    )}
   </div>  
-)
+)}
 
 export default Blog
