5580d82e76a2fcff9a47c74d5397189f3e6276a9
W-oland <tatu.jarvenranta@gmail.com>
2021-09-08 08:53:34 +0300

    tehtavat 6.13-14

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NewAnecdote from './components/AnecdoteForm'
 import Anecdotes from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import anecdoteService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+import axios from 'axios'
 
 const App = () => {
+
+  const dispatch = useDispatch()
+  
+  const hook = () => {
+    console.log('effect')
+    anecdoteService.getAll().then(anecdotes =>
+      dispatch(initializeAnecdotes(anecdotes))      
+    )
+  }
+  useEffect(hook,[dispatch])
 
   return (
     <div>
--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { showNotification, hideNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const NewAnecdote = (props) => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
-        dispatch(createAnecdote(content))
+        const newAnecdote = await anecdoteService.createNew(content)
+        dispatch(createAnecdote(newAnecdote))
+        
         const message = `you added ${content}`
         dispatch(showNotification(message))
         setTimeout(() => {
