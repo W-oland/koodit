57b1830b4ceeee7cea2944c7d8a69dae8af0b15c
Tatu Jarvenranta <tatu.jarvenranta@valtiokonttori.fi>
2021-09-14 14:51:21 +0300

    tehtavat osioon 7

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, Redirect} from 'react-router-dom'
 
 const Menu = () => {
+  
   const padding = {
     paddingRight: 5
   }
+
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to="/" >anecdotes</Link>
+      <Link style={padding} to="/create" >create new</Link>
+      <Link style={padding} to="/about" >about</Link>
     </div>
   )
 }
@@ -17,10 +20,23 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote =>
+         <li key={anecdote.id} >
+           <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+           </li>)}
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdote }) => {
+  return (
+    <div>
+      <h2>{anecdote.content} by {anecdote.author}</h2>
+      <div>has {anecdote.votes} votes</div>
+      for more info see <a href={anecdote.info}>{anecdote.info}</a>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -58,6 +74,7 @@ const CreateNew = (props) => {
       info,
       votes: 0
     })
+    
   }
 
   return (
@@ -106,6 +123,10 @@ const App = () => {
   const addNew = (anecdote) => {
     anecdote.id = (Math.random() * 10000).toFixed(0)
     setAnecdotes(anecdotes.concat(anecdote))
+    setNotification(`a new anecdote ${anecdote.content} created!`)
+    setTimeout(() => {
+      setNotification(null)
+    }, 10000)
   }
 
   const anecdoteById = (id) =>
@@ -122,13 +143,31 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const match = useRouteMatch('/anecdotes/:id')
+  
+  const anecdote = match
+    ? anecdotes.find(anecdote => anecdote.id === match.params.id)
+    : null
+
   return (
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <div><h2>{notification}</h2></div>
+      <Switch>
+        <Route path="/anecdotes/:id">
+          <Anecdote anecdote={anecdote} />
+        </Route>
+        <Route path="/create">
+          {notification ? <Redirect to="/" /> : <CreateNew addNew={addNew} />}
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <AnecdoteList anecdotes={anecdotes} />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   )
