a0d2b9068cc29597ec0a1e99fb0eaf64bd4ea98a
W-oland <tatu.jarvenranta@gmail.com>
2021-10-22 12:46:50 +0300

    tehtava

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,6 +1,11 @@
-interface Numbers {
+interface Input {
+    target: number;
+    hours: Array<number>;
+}
+
+interface Output {
     periodLength: number;
-    traningDays: number;
+    trainingDays: number;
     success: boolean;
     rating: number;
     ratingDescription: string;
@@ -8,26 +13,50 @@ interface Numbers {
     average: number;
 }
 
-const parseArguments = (args: Array<string>): Numbers => {
-    if (args.length < 4) throw new Error('Not enough arguments');
-    if (args.length > 4) throw new Error('Too many arguments');
+const parser = (args: Array<string>): Input => {
+    if (args.length < 4) throw new Error('Too few arguments');
+    const hours = args.slice(3).map(hours => Number(hours)) // <-- type casting string array into number array
 
-    if (
-        !isNaN(Number(args[2]))
-        && !isNaN(Number(args[3]))
-        && !isNaN(Number(args[5]))
-        && !isNaN(Number(args[7]))
-        && !isNaN(Number(args[8]))
-    ) {
+    if (!hours.some(isNaN) && !isNaN(Number(args[2]))) {
         return {
-            periodLength: Number(args[2]),
-            traningDays: Number(args[3]),
-            success: Boolean(args[4]),
-            rating: Number(args[5]),
-            ratingDescription: args[6],
-            target: Number(args[7]),
-            average: Number(args[8])
+            target: Number(args[2]),
+            hours: hours,
         }
+    } else {
+        throw new Error ('Malformatted values. Insert numbers only.')
     }
-    }
+}
+
+const counter = (first: number, second: Array<number>): Output => {
+    const periodLength = second.length;
+    const trainingDays = second.filter(value => value > 0).length;
+    const average = second.reduce( (a,b) => (a+b) / second.length ); // <-- used in the next line
+    const success = (first >= average) ? true : false ;
+    const rating = (first > average) ? 1 
+        : (first = average) ? 2 
+        : (first < average) ? 3 
+        : 0;
+    const ratingDescription = (first > average) ? 'Bad' 
+    : (first = average) ? 'Better' 
+    : (first < average) ? 'Best' 
+    : 'Worst';
+
+    const target = first;
+
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average,
+    };
+}
+
+try {
+    const { target, hours } = parser(process.argv);
+    console.log(counter(target, hours));
+} catch (e) {
+    console.log('Error, something bad happened, message: ', e.message)
 }
